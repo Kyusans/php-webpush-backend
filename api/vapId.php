b0511cb2aee47a987afc7e34254ff3f44c4427f1
Mel Macario <124644863+Kyusans@users.noreply.github.com>
2023-11-01 10:50:16 +0800

    fixed: can generate public and private key now

--- api/vapId.php
+++ api/vapId.php
@@ -5,4 +5,9 @@ use Minishlink\WebPush\VAPID;
 
 $vapidKeys = VAPID::createVapidKeys();
 
-print_r($vapidKeys);
+var_dump(VAPID::createVapidKeys());
+
+/* 
+  ["publicKey"]=> BA-Nqq2lYA6oDxHWLf-He6WjjjwQp-A_k_pGwBHg2vx8S0Nu-bKI1zD-WUpnDywVmJ8KCMZvmVxfhBtHPTQwqbE
+  ["privateKey"]=> M_mjCj0IkN-D1tq0RyPd3uISOXSJkjJrLVwZbYyKx0Y
+*/
