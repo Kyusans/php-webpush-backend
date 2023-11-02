13e2edc786f4df58ab52e323b939982e86ab1c9b
Mel Macario <124644863+Kyusans@users.noreply.github.com>
2023-11-02 08:16:38 +0800

    Update vapId.php

--- api/vapId.php
+++ api/vapId.php
@@ -1,12 +1,7 @@
 <?php
 require_once("vendor/autoload.php");
-
 use Minishlink\WebPush\VAPID;
-
-$vapidKeys = VAPID::createVapidKeys();
-
 var_dump(VAPID::createVapidKeys());
-
 /* 
   ["publicKey"]=> BOzeUpvZ9K0cw0v4Us7gad1C11aMEwmxmF95_gb6_y7FP_aQMY2F7aFU3z8vqa-c1M6FSdfW5bTXPoMS1k0pNIc
   ["privateKey"]=> qvL8murvzGZtXGlrheqo6-96ZRjl1yxLCiDiNhDf98k
